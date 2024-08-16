b839076157e188d74afa18cd1a2a098f87e96414
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com.>
2024-08-16 16:18:28 +1000

    Added command parser and better exit
    Now eats <C-c> and only exits with exit

--- mysh.py
+++ mysh.py
@@ -1,11 +1,46 @@
 import os
 
+class myExit(Exception):
+    def __init__(self, errorCode):
+        self.code = errorCode
+
+def parseInput(data: str) -> (str, [str], [str]):
+    """returns (command, args, flags)"""
+    data = data.split()
+    command, data = data[0].lower(), data[1:]
+
+    args = []
+    flags = []
+
+    for item in data:
+        if item.startswith("-"):
+            flags.append(item)
+        else: 
+            args.append(item)
+
+    return command, args, flags
+
 def main():
     if not (prompt := os.environ.get("PROMPT")):
         prompt = ">> "
 
     while True:
-        command = input(f"{prompt} ")
+        try:
+            loop(prompt)
+        except KeyboardInterrupt:
+            continue
+        except myExit as e:
+            os._exit(e.code)
+
+
+def loop(prompt):
+    while True:
+        data = input(f"{prompt} ")
+        command, args, flags = parseInput(data)
+
+        if command == "exit":
+            raise myExit(0)
+
 
 
 if __name__ == "__main__":
