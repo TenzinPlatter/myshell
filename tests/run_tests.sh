33583cbb8aaf2ac2f21e504a4a81e212b3635bbb
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2024-09-05 18:20:07 +1000

    Added test cases and script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,15 +9,80 @@ else
 	tests_dir="${cwd}/tests"
 fi
 
-curr_out="${tests_dir}/curr.out"
+mysh_path="${tests_dir}/../mysh.py"
 
-exit_dir="${tests_dir}/exit_tests"
+last_diff="${tests_dir}/curr.out"
 
-diff <(python3 mysh.py < "${exit_dir}/exit.in") "${exit_dir}/exit.out" > ${curr_out}
 
-if ${curr_test_diff}; then
-	echo "Exit test (No args) passed"
+# BUILTIN SECTION
+builtin_dir="${tests_dir}/builtin_tests"
+
+echo
+
+if diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/exit.in") "${builtin_dir}/exit.out" > /dev/null; then
+	echo "Exit test passed (No args)"
 else
-	echo "Exit test (No args) failed, see diff:"
-	diff <(python3 mysh.py < "${io_files_dir}/exit.in") "${io_files_dir}/exit.out"
+	echo "Exit test failed (No args), see diff:"
+	diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/exit.in") "${builtin_dir}/exit.out"
 fi
+
+echo
+
+if diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/which1.in") "${builtin_dir}/which1.out" > /dev/null; then
+	echo "Which test 1 passed (1 arg)"
+else
+	echo "Which test 1 failed, see diff:"
+	diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/which1.in") "${builtin_dir}/which1.out"
+fi
+
+echo
+
+if diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/which2.in") "${builtin_dir}/which2.out" > /dev/null; then
+	echo "Which test 2 passed (2 arg)"
+else
+	echo "Which test 2 failed, see diff:"
+	diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/which2.in") "${builtin_dir}/which2.out"
+fi
+
+echo
+
+if diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/pwd.in") "${builtin_dir}/pwd.out" > /dev/null; then
+	echo "pwd test passed"
+else
+	echo "pwd test failed, see diff:"
+	diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/pwd.in") "${builtin_dir}/pwd.out"
+fi
+
+echo
+
+if diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/cd.in") "${builtin_dir}/cd.out" > /dev/null; then
+	echo "cd test passed"
+else
+	echo "cd test failed, see diff:"
+	diff --ignore-trailing-space <(python3 ${mysh_path} < "${builtin_dir}/cd.in") "${builtin_dir}/cd.out"
+fi
+
+echo
+
+
+# EXECUTABLES SECTION
+executable_dir="${tests_dir}/executable_tests"
+
+if diff --ignore-trailing-space <(python3 ${mysh_path} < "${executable_dir}/echo.in") "${executable_dir}/echo.out" > /dev/null; then
+	echo "Echo test passed"
+else
+	echo "Echo test failed, see diff:"
+	diff --ignore-trailing-space <(python3 ${mysh_path} < "${executable_dir}/echo.in") "${executable_dir}/echo.out"
+fi
+
+echo
+
+if diff --ignore-trailing-space <(python3 ${mysh_path} < "${executable_dir}/exec.in") "${executable_dir}/exec.out" > /dev/null; then
+	echo "Executable test passed"
+else
+	echo "Try running testing script from root dir"
+	echo "Executable test failed, see diff:"
+	diff --ignore-trailing-space <(python3 ${mysh_path} < "${executable_dir}/exec.in") "${executable_dir}/exec.out"
+fi
+
+echo
